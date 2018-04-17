2b13d583ae511e93f3084aec6f830fd3d142d9ab
oscardoudou <oscar9591@icloud.com>
2018-04-16 21:43:45 -0400

    add occupancy menu

--- src/OccupancyReport.java
+++ src/OccupancyReport.java
@@ -20,6 +20,46 @@ import java.util.Scanner;
  */
 
 public class OccupancyReport {
+
+    public static void printMenu(){
+        System.out.println("1.Get Occupancy by Hotel");
+        System.out.println("2.Get Occupancy by Room Category");
+        System.out.println("3.Get Occupancy by City");
+        System.out.println("4.Get Occupancy by DateRange");
+        System.out.println("5.Return to Main Menu");
+    }
+    public static void openMenu(){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            printMenu();
+            System.out.print("\nEnter Selection: ");
+            String choice = sc.nextLine();
+
+            switch (choice){
+                case "1":
+                    getHotelOccupancy();
+                    break;
+                case "2":
+                    getRoomTypeOccupancy();
+                    break;
+                case "3":
+                    getCityOccupancy();
+                    break;
+                case "4":
+                    getDataRangeOccupancy();
+                    break;
+                case "5":
+                    System.out.println("Returning to Main Menu...");
+                    return;
+                default:
+                    System.out.println("Invalid Entry");
+                    break;
+
+            }
+        }
+
+    }
+
     public static void getHotelOccupancy(){
         String sql = "select hotel_id, count(*) as occupancy from Room where avai = 0 group by hotel_id";
         Connection conn =  DBConnection.getConnection();
